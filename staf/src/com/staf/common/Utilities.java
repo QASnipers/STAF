044dac3ac4930e3874f80ed8ca4d4a2ce890a3ae
Murali Kunda <murali.kunda@gmail.com>
2014-10-06 17:17:47 -0400

    Adding comments

--- staf/src/com/staf/common/Utilities.java
+++ staf/src/com/staf/common/Utilities.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebElement;
 
 public class Utilities {
 	
+/*	Author: Murali Kunda
+*/	
 	public static void waitForElement(WebElement element, int seconds) {
 		long end = System.currentTimeMillis() + (seconds * 1000);
 		while (System.currentTimeMillis() < end) {
