5859042de93e04007ed3e2cbf75808cff06f0652
murali.kunda@gmail.com <murali.kunda@gmail.com>
2018-06-22 19:06:41 -0400

    Removed excel reading code from Application class

--- staf/src/com/staf/common/Application.java
+++ staf/src/com/staf/common/Application.java
@@ -1,44 +1,18 @@
 package com.staf.common;
 
-import java.io.File;
 
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.staf.pages.*;
-import com.staf.reader.ConfigReader;
-import com.staf.reader.ExcelReader;
 
 public class Application {
+	//Register new pages here
 	public Browser browser = new Browser();
-	public Navigate Navigate  = new Navigate() ;
 	public Login Login = new Login();
+	public NavigateTo NavigateTo  = new NavigateTo() ;
+	public OrgandHiringType OrgandHiringType = new OrgandHiringType();
+
+	
 	
 	public Application() throws Exception {		
-		//ConfigReader.getInstance().readConfiguration();
-		//String testDataFile = ConfigReader.getInstance().getTestDataFilesPath() +
-		String testDataFile = System.getProperty("user.dir") +"//TestData//"+ 							  
-							    ConfigReader.getInstance().getTestCaseName()+".xls";
-		String sheetName = ConfigReader.getInstance().getEnvironment();
 		
-		int rowCount = ExcelReader.getDataRowCount(testDataFile, sheetName);
-		  if(rowCount <= 0){
-
-			  System.out.println("No column names or data found in the data file. File Name: "+ testDataFile 
-					  			 + " Sheet Name: "+sheetName);
-		  }else if(rowCount == 1){
-			  rowCount = 0;
-		  }
-		  else {
-			  ConfigReader.getInstance().setDataRowsCount(rowCount-2);
-		  }
 	}
-	
-	
-	
-
 }
